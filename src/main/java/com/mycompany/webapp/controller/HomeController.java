1381a4fe3c72c64cfe7fca49639a08b6dd7da665
seoyoung78 <tjdud0708@naver.com>
2021-07-01 18:01:03 +0900

    삭제

--- src/main/java/com/mycompany/webapp/controller/HomeController.java
+++ src/main/java/com/mycompany/webapp/controller/HomeController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mycompany.webapp.mqtt.MqttTemplate;
-import com.mycompany.webapp.mqtt.MqttTemplate2;
 
 @CrossOrigin(origins="*")
 @Controller
@@ -26,67 +25,12 @@ public class HomeController {
 	private RedisTemplate<String, String> redisTemplate;
 	
 	@Autowired
-	private MqttTemplate2 mqttTemplate;
+	private MqttTemplate mqttTemplate;
 	
 	@RequestMapping("/home")
 	public String home() {
 		return "home";
 	}
-	
-	@RequestMapping("/chat2")
-	public String chat2() {
-		return "chat2";
-	}
-	
-	@RequestMapping("/redis2")
-	public String redis2() {
-		return "redis2";
-	}
-
-	@RequestMapping("/sendRedisMessage2")
-	public void sendRedisMessage(String topic, String content, HttpServletResponse response) {
-		try {
-			//Redis로 메시지를 보내면 WebSocket로 전달
-			redisTemplate.convertAndSend(topic, content);
-			
-			response.setContentType("application/json; charset=UTF-8");
-			PrintWriter pw = response.getWriter();
-			
-			JSONObject jsonObject = new JSONObject();
-			jsonObject.put("result", "success");
-			pw.println(jsonObject.toString());	//{"result":"success"}
-			pw.flush();
-			pw.close();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	@RequestMapping("/mqtt2")
-	public String mqtt2() {
-		return "mqtt2";
-	}
-	
-	@RequestMapping("/sendMqttMessage2")
-	public void sendMqttMessage2(String topic, String content, HttpServletResponse response) {
-		try {
-			//Redis로 메시지를 보내면 WebSocket로 전달
-			mqttTemplate.sendMessage(topic, content);
-			
-			response.setContentType("application/json; charset=UTF-8");
-			PrintWriter pw = response.getWriter();
-			
-			JSONObject jsonObject = new JSONObject();
-			jsonObject.put("result", "success");
-			pw.println(jsonObject.toString());	//{"result":"success"}
-			pw.flush();
-			pw.close();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	//------------------------------------------------------------------------------------------------
 	
 	@RequestMapping("/chat")
 	public String chat() {
@@ -103,39 +47,39 @@ public class HomeController {
 		return "mqtt";
 	}
 	
-//	@RequestMapping("/sendRedisMessage")
-//	public void sendRedisMessage(String topic, String content, HttpServletResponse res) {
-//		logger.info("sendMessage");
-//		try {
-//			redisTemplate.convertAndSend(topic, content);
-//		
-//			JSONObject json = new JSONObject();
-//			json.put("result", "success");
-//			res.setContentType("application/json; charset=UTF-8");
-//			PrintWriter writer = res.getWriter();
-//			writer.write(json.toString());
-//			writer.flush();
-//			writer.close();
-//		} catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	@RequestMapping("/sendMqttMessage")
-//	public void sendMqttMessage(String topic, String content, HttpServletResponse res) {
-//		logger.info("sendMessage");
-//		try {
-//			mqttTemplate.sendMessage(topic, content);
-//		
-//			JSONObject json = new JSONObject();
-//			json.put("result", "success");
-//			res.setContentType("application/json; charset=UTF-8");
-//			PrintWriter writer = res.getWriter();
-//			writer.write(json.toString());
-//			writer.flush();
-//			writer.close();
-//		} catch(Exception e) {
-//			e.printStackTrace();
-//		}
-//	}	
+	@RequestMapping("/sendRedisMessage")
+	public void sendRedisMessage(String topic, String content, HttpServletResponse res) {
+		logger.info("sendMessage");
+		try {
+			redisTemplate.convertAndSend(topic, content);
+		
+			JSONObject json = new JSONObject();
+			json.put("result", "success");
+			res.setContentType("application/json; charset=UTF-8");
+			PrintWriter writer = res.getWriter();
+			writer.write(json.toString());
+			writer.flush();
+			writer.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@RequestMapping("/sendMqttMessage")
+	public void sendMqttMessage(String topic, String content, HttpServletResponse res) {
+		logger.info("sendMessage");
+		try {
+			mqttTemplate.sendMessage(topic, content);
+		
+			JSONObject json = new JSONObject();
+			json.put("result", "success");
+			res.setContentType("application/json; charset=UTF-8");
+			PrintWriter writer = res.getWriter();
+			writer.write(json.toString());
+			writer.flush();
+			writer.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}	
 }
--- src/main/webapp/WEB-INF/views/common/menu.jsp
+++ src/main/webapp/WEB-INF/views/common/menu.jsp
@@ -17,18 +17,6 @@
     </a>
   </li>
   
-  <li class="nav-item mb-2">
-    <h6 class="text-white">연습 소스</h6>
-    <a class="nav-link text-warning" href="<%=application.getContextPath()%>/chat2">
-    	WebSocket
-    </a>
-    <a class="nav-link text-warning" href="<%=application.getContextPath()%>/redis2">
-    	WebSocket + Redis
-    </a>
-    <a class="nav-link text-warning" href="<%=application.getContextPath()%>/mqtt2">
-    	MQTT
-    </a>
-  </li>
 </ul>
 
 
